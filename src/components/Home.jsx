67efc5760e0589f244dcd4a9d9746f73e6489346
merwhite11 <meredith.white91@gmail.com>
2023-12-01 16:12:50 -0800

    Attempt with sample image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import WritingContainer from './writing/WritingContainer.jsx';
 import DanceContainer from './dance/DanceContainer.jsx';
+import PDFFile from './PDFFile.js';
 import {Link} from 'react-router-dom';
 
 const Home = (props) => {
@@ -9,6 +10,7 @@ const Home = (props) => {
           <div className="row justify-content-center">
             <WritingContainer></WritingContainer>
             <DanceContainer></DanceContainer>
+            <PDFFile></PDFFile>
           </div>
         </div>
       );
