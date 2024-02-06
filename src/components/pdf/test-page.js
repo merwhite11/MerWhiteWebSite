73f268b75458b5c5a8bb88832e45d9e126a2706a
merwhite11 <meredith.white91@gmail.com>
2024-02-06 12:54:54 -0800

    Added change font btn, not functional

--- src/components/pdf/test-page.js
+++ src/components/pdf/test-page.js
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import { ReactReader, EpubView } from 'react-reader';
 import { Container } from 'react-bootstrap';
 import useLocalStorageState from 'use-local-storage-state';
+import { Contents, Rendition } from 'epubjs'
 
 import { DEMO_URL } from '../config';
 
@@ -9,18 +10,37 @@ import { DEMO_URL } from '../config';
 const TestPage = ({ doc, title }) => {
 
   const epubUrl = process.env.PUBLIC_URL + `${doc}`;
-  const [location, setLocation] = useLocalStorageState('book-loc', 0)
+  const [location, setLocation] = useLocalStorageState('book-loc', 0);
+  const rendition = useRef(undefined)
+  // const [largeText, setLargeText] = useState(false);
+  const [size, setSize] = useState(100)
+
+  const changeSize = newSize => {
+    setSize(newSize)
+  }
+
+  useEffect(() => {
+    rendition.current?.themes.fontSize(`${size}%`)
+  }, [size])
 
   return (
     <div className="reader-container">
+      <div className="font-size-btn">
+        <button onClick={() => changeSize(Math.max(80, size - 10))}>-</button>
+        <span>Current size: {size}%</span>
+        <button onClick={() => changeSize(Math.max(80, size + 10))}>+</button>
+      </div>
 
-      <ReactReader className="reader"
+      < ReactReader className="reader"
         title={title}
         url={epubUrl}
         location={location}
-        locationChanged = {(loc) => setLocation(loc)}
+        locationChanged={(loc) => setLocation(loc)}
+
       />
     </div>
+    // </div>
+    // </div>
   )
 }
 
