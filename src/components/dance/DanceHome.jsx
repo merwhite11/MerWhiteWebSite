bae4b5c4b560f436c3a0811d15b60bfb62c2de43
merwhite11 <meredith.white91@gmail.com>
2024-02-28 18:26:52 -0800

    Saving changes on Dance Home

--- src/components/dance/DanceHome.jsx
+++ src/components/dance/DanceHome.jsx
@@ -21,22 +21,6 @@ class DanceHome extends React.Component {
     _onReady(event) {
         event.target.pauseVideo();
     }
-}
-
-// const DanceHome = (props) => {
-//     return (
-//         <div className="col-md-4">
-//             <div className="card">
-//                 <div className="card-body">
-//                     <h2 className="card-title">DANCE HOME</h2>
-//                     <p className="card-text">Closer</p>
-//                     <p className="card-text">Pool</p>
-//                     <a href='#' className="btn btn-primary">WATCH</a>
-//                 </div>
-//              </div>
-//         </div>
-
-//     )
-// };
+};
 
 export default DanceHome;
