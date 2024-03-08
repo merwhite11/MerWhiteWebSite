0918a8a7958c06d1965d5549b2d782186e2d8e89
merwhite11 <meredith.white91@gmail.com>
2024-03-08 14:03:10 -0800

    Changed routes for NavBar

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -3,15 +3,21 @@ import { useNavigate, BrowserRouter as Router, Routes, Route } from 'react-route
 import {Navbar, Nav, Container} from 'react-bootstrap';
 
 const AppNavbar = () => {
+   //needs to be triggered by a user action
+   const navigate = useNavigate();
+
+   const handleClick = (path) => {
+       navigate(`/${path}`)
+   }
   return (
     <Navbar className="nav-bar col-12" data-bs-theme="light">
     <Container>
       <Navbar.Collapse id="basic-navbar-nav" ></Navbar.Collapse>
       <Nav className="justify-content-end">
-      <Nav.Link className="nav-bar" href="/">Home</Nav.Link>
-        <Nav.Link className="nav-bar" href="/writing">Writing</Nav.Link>
-        <Nav.Link className="nav-bar" href="/dance">Dance</Nav.Link>
-        <Nav.Link className="nav-bar" href="/contact">Who's She?</Nav.Link>
+      <Nav.Link className="nav-bar" onClick={(e)=>handleClick('/')}>Home</Nav.Link>
+        <Nav.Link className="nav-bar" onClick={(e)=>handleClick('/writing')}>Writing</Nav.Link>
+        <Nav.Link className="nav-bar" onClick={(e)=>handleClick('/dance')}>Dance</Nav.Link>
+        <Nav.Link className="nav-bar" onClick={(e)=>handleClick('/contact')}>Who's She?</Nav.Link>
       </Nav>
     </Container>
     </Navbar>
